336403ffbec1b7dcce2b25f1ac5334c92e3d03a1
Maria <maria>
2024-03-18 21:55:15 +0000

    inserts user

---
+++ db_scripts/populate.sql
@@ -0,0 +1,16 @@
+-- Add Users
+insert into users (username, password) values ('maria', 'maria123');
+insert into users (username, password) values ('joao', 'joao123');
+insert into users (username, password) values ('afonso', 'afonso123');
+insert into users (username, password) values ('roberta', 'roberta123');
+insert into users (username, password) values ('rodrigo', 'rodrigo123');
+insert into users (username, password) values ('miguel', 'miguel123');
+insert into users (username, password) values ('cesar', 'cesar123');
+insert into users (username, password) values ('laura', 'laura123');
+insert into users (username, password) values ('joana', 'joana123');
+insert into users (username, password) values ('bernardo', 'bernardo123');
+insert into users (username, password) values ('sofia', 'sofia123');
+insert into users (username, password) values ('andre', 'andre123');
+insert into users (username, password) values ('goncalo', 'goncalo123');
+insert into users (username, password) values ('marta', 'marta123');
+insert into users (username, password) values ('martim', 'martim123');
