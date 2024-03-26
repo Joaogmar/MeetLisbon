08b66f800e42734a79b4715c3415dcac18507354
Maria <maria>
2024-03-26 22:26:56 +0000

    correção da bd

--- db_scripts/bd.sql
+++ db_scripts/bd.sql
@@ -1,34 +1,17 @@
 CREATE TABLE users (
-    user_id SERIAL NOT NULL CONSTRAINT user_pkey PRIMARY KEY,
-    username VARCHAR NOT NULL,
-    role VARCHAR NOT NULL,
-    password VARCHAR NOT NULL
+    user_id SERIAL NOT NULL CONSTRAINT user_pkey PRIMARY KEY, username VARCHAR NOT NULL, role VARCHAR NOT NULL, password VARCHAR NOT NULL
 );
 
 CREATE TABLE poi (
-    location_id SERIAL NOT NULL CONSTRAINT location_pkey PRIMARY KEY,
-    location_name VARCHAR NOT NULL,
-    location_address VARCHAR NOT NULL,
-    longitude DECIMAL NOT NULL,
-    latitude DECIMAL NOT NULL,
-    info TEXT,
-    image_url VARCHAR
+    location_id SERIAL NOT NULL CONSTRAINT location_pkey PRIMARY KEY, location_name VARCHAR NOT NULL, location_address VARCHAR NOT NULL, longitude DECIMAL NOT NULL, latitude DECIMAL NOT NULL, info TEXT, image_url VARCHAR
 );
 
 CREATE TABLE favorite_routes (
-    user_id INT NOT NULL,
-    route_id SERIAL NOT NULL CONSTRAINT route_pkey PRIMARY KEY,
-    route_name VARHAR NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    PRIMARY KEY (user_id, route_id)
+    user_id INT NOT NULL, route_id INT NOT NULL, route_name VARCHAR NOT NULL, FOREIGN KEY (user_id) REFERENCES users (user_id), PRIMARY KEY (user_id, route_id)
 );
 
 CREATE TABLE route (
-    route_id INT NOT NULL,
-    poi_id INT NOT NULL,
-    FOREIGN KEY (route_id) REFERENCES favorite_routes(route_id),
-    FOREIGN KEY (poi_id) REFERENCES poi(location_id),
-    PRIMARY KEY (route_id, poi_id)
+    route_id INT NOT NULL, poi_id INT NOT NULL, FOREIGN KEY (route_id) REFERENCES favorite_routes (route_id), FOREIGN KEY (poi_id) REFERENCES poi (location_id), PRIMARY KEY (route_id, poi_id)
 );
 
 -- add wishlist de places
