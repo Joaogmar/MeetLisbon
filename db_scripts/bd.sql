cbcfa7ee453e5924bca7ca1f689a81219d68cbe6
RobertaSantos11 <99985296+RobertaSantos11@users.noreply.github.com>
2024-10-21 21:50:56 +0100

    Update bd.sql

--- db_scripts/bd.sql
+++ db_scripts/bd.sql
@@ -21,7 +21,8 @@ CREATE TABLE poi (
 CREATE TABLE favorite_routes (
     user_id INT NOT NULL,
     route_name VARCHAR NOT NULL,
-    route_points INT NOT NULL, 
+    route_points INT NOT NULL,
+    fr_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, route_name)
 );
@@ -29,7 +30,22 @@ CREATE TABLE favorite_routes (
 CREATE TABLE favorite_places (
     user_id INT NOT NULL,
     poi_id INT NOT NULL,
+    fp_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
     FOREIGN KEY (poi_id) REFERENCES poi (location_id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, poi_id)
+);
+CREATE TABLE Route_History (
+    route_id INT NOT NULL,
+    rh_action VARCHAR NOT NULL,
+    date TIMESTAMP NOT NULL 
+    metrica VARCHAR NOT NULL,
+    user_id INT NOT NULL,
+    poi_id INT NOT NULL,
+    fp_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (fr_id) REFERENCES favorite_routes (fr_id) ON DELETE CASCADE,
+    FOREIGN KEY (fp_id) REFERENCES favorite_places (fp_id) ON DELETE CASCADE,
+    FOREIGN KEY (poi_id) REFERENCES poi (location_id) ON DELETE CASCADE,
+    PRIMARY KEY (route_id)
 );
