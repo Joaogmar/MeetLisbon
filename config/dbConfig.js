c396b9cec2265aef4b0625a5972d6b6994f02c4a
JoaoMarques03 <jgmarques03@gmail.com>
2024-11-02 18:02:15 +0000

    updated db location and project structure

---
+++ config/dbConfig.js
@@ -0,0 +1,25 @@
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    client.query('SELECT NOW()', (err, result) => {
+        release();
+        if (err) {
+            return console.error('Error executing query', err.stack);
+        }
+        console.log('Connection successful:', result.rows);
+    });
+});
+
+module.exports = pool;
